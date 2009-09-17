632c4a1e93876bb38bb420c89a3810dac72807b9
ma.danbo <ma.danbo@e1d36b18-a32f-11de-bf1c-ef771b8a44ac>
2009-09-17 03:15:20 +0000

    Player2 patterned after DumbPlayer1

---  explorandum-g2 --username ma.danbo/Explorandum/src/explorandum/f09/g2/Player2.java
+++  explorandum-g2 --username ma.danbo/Explorandum/src/explorandum/f09/g2/Player2.java
@@ -2,35 +2,39 @@ package explorandum.f09.g2;
 
 import java.awt.Color;
 import java.awt.Point;
+import java.util.ArrayList;
 import java.util.Random;
 
 import explorandum.Logger;
 import explorandum.Move;
 import explorandum.Player;
 
-public class LumberingTroglodyte implements Player {
+public class Player2 implements Player {
+	ArrayList<Point> CellMemory;
+	Random rand;
 
 	public Color color() throws Exception {
 		// TODO Auto-generated method stub
-		return null;
+		return Color.GREEN;
 	}
 
 	public Move move(Point currentLocation, Point[] offsets,
 			Boolean[] hasExplorer, Integer[][] visibleExplorers,
 			Integer[] terrain, int time, Boolean StepStatus) throws Exception {
 		// TODO Auto-generated method stub
-		return null;
+		int action = ACTIONS[rand.nextInt(ACTIONS.length)];
+		return new Move(action);
 	}
 
 	public String name() throws Exception {
 		// TODO Auto-generated method stub
-		return null;
+		return "Lumbering Troglodyte";
 	}
 
 	public void register(int explorerID, int rounds, int explorers, int range,
 			Logger log, Random rand) {
-		// TODO Auto-generated method stub
-
+		CellMemory = new ArrayList<Point>();
+		log.debug("\nRounds:" + rounds + "\nExplorers:" + explorers + "\nRange:" + range);
 	}
 
 }
