e91e0e588ddf011113687b905205b3a718f593fc
ma.danbo <ma.danbo@e1d36b18-a32f-11de-bf1c-ef771b8a44ac>
2009-09-17 13:53:21 +0000

    minor changes, but random movement isn't working yet for some reason

--- trunk/ explorandum-g2 --username ma.danbo/Explorandum/src/explorandum/f09/g2/Player2.java
+++ trunk/ explorandum-g2 --username ma.danbo/Explorandum/src/explorandum/f09/g2/Player2.java
@@ -2,8 +2,11 @@ package explorandum.f09.g2;
 
 import java.awt.Color;
 import java.awt.Point;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Properties;
 
 import explorandum.Logger;
 import explorandum.Move;
@@ -12,29 +15,34 @@ import explorandum.Player;
 public class Player2 implements Player {
 	ArrayList<Point> CellMemory;
 	Random rand;
+	Logger log;
+
 
 	public Color color() throws Exception {
-		// TODO Auto-generated method stub
 		return Color.GREEN;
 	}
 
+	
 	public Move move(Point currentLocation, Point[] offsets,
 			Boolean[] hasExplorer, Integer[][] visibleExplorers,
 			Integer[] terrain, int time, Boolean StepStatus) throws Exception {
-		// TODO Auto-generated method stub
+		// TODO create custom action
 		int action = ACTIONS[rand.nextInt(ACTIONS.length)];
 		return new Move(action);
 	}
 
+	
 	public String name() throws Exception {
-		// TODO Auto-generated method stub
 		return "Lumbering Troglodyte";
 	}
 
+	
 	public void register(int explorerID, int rounds, int explorers, int range,
-			Logger log, Random rand) {
+			Logger _log, Random rand) {
 		CellMemory = new ArrayList<Point>();
+		log = _log;
 		log.debug("\nRounds:" + rounds + "\nExplorers:" + explorers + "\nRange:" + range);
 	}
+
 
 }
