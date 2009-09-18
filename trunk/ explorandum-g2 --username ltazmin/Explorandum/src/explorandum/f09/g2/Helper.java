75718d57e89f5131a00c50b01ba9db4c871c977a
ltazmin <ltazmin@e1d36b18-a32f-11de-bf1c-ef771b8a44ac>
2009-09-18 00:58:18 +0000

    Adding helper class for various static functions

---
+++ trunk/ explorandum-g2 --username ltazmin/Explorandum/src/explorandum/f09/g2/Helper.java
@@ -0,0 +1,49 @@
+package explorandum.f09.g2;
+
+import java.awt.Point;
+
+/**
+ * Helper class that supports scoring and path finding etc.
+ * @author laima
+ *
+ */
+public class Helper {
+	
+	/**
+	 * Calculates score
+	 * @param p Point object of current cell
+	 * @return score
+	 */
+	public static int getCellScore(Point p) {
+		
+		Cell cell;
+		return 0;
+		
+		
+	}
+	
+	/**
+	 * Calculates best path
+	 * @param p1 
+	 * @param p2
+	 * @return path array
+	 */
+	public static Point[] getPath(Point p1, Point p2) {
+		
+		return null;
+		
+	}
+	
+	/**
+	 * Calculates path score based on path array
+	 * @param pArray 
+	 * @return score
+	 */
+	public static int getPathScore(Point[] pArray) {
+		
+		return 0;
+	}
+	
+	
+
+}
