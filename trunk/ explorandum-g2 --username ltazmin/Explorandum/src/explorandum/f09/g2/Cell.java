89d04886d00b5185c11eb74e87d311ebdb9186c4
sharadh <sharadh@e1d36b18-a32f-11de-bf1c-ef771b8a44ac>
2009-09-19 23:52:03 +0000

    Added comments to all classes Restructured some objects, methods

--- trunk/ explorandum-g2 --username ltazmin/Explorandum/src/explorandum/f09/g2/Cell.java
+++ trunk/ explorandum-g2 --username ltazmin/Explorandum/src/explorandum/f09/g2/Cell.java
@@ -2,67 +2,115 @@ package explorandum.f09.g2;
 
 import java.awt.Point;
 
+/**
+ * Class representing each cell of the map along with information collected
+ * about it
+ * 
+ * @author sharadh
+ * 
+ */
 public class Cell {
-
-	public static final int OWNERSHIP_UNKNOWN = 0;
-	public static final int OWNERSHIP_US = 1;
-	public static final int OWNERSHIP_THEM = 2;
-
-	public static final int TERRAIN_UNKNOWN = -1;
-	public static final int TERRAIN_LAND = 0;
-	public static final int TERRAIN_MOUNTAIN = 2;
-	public static final int TERRAIN_WATER = 1;
 
 	private Point p;
 
-	private int terrain = TERRAIN_UNKNOWN;
-	private int whoOwns = OWNERSHIP_UNKNOWN;
+	private int terrain = Constants.TERRAIN_UNKNOWN;
+	private int whoOwns = Constants.OWNED_BY_UNKNOWN;
 	private boolean isVisited = false;
-	private int visitationTurn = -1;
-	private int visitationCount = 0;
+	private int turnVisited = -1;
+	private int noOfTimesVisited = 0;
 
 	public int getVisitationTurn() {
-		return visitationTurn;
+		return turnVisited;
 	}
 
-	public void setVisitationTurn(int visitationTurn_) {
-		visitationTurn = visitationTurn_;
+	/**
+	 * Sets the
+	 * 
+	 * @param turnVisited_
+	 */
+	public void setVisitationTurn(int turnVisited_) {
+		turnVisited = turnVisited_;
 	}
 
+	/**
+	 * REturns true if this cell has been visited by player
+	 * 
+	 * @return
+	 */
 	public boolean isVisited() {
 		return isVisited;
 	}
 
+	/**
+	 * Sets this as a cell visited by explorer
+	 * 
+	 * @param isVisited_
+	 */
 	public void setVisited(boolean isVisited_) {
 		isVisited = isVisited_;
 	}
 
+	/**
+	 * Constructor
+	 * 
+	 * @param p_
+	 */
 	public Cell(Point p_) {
 		super();
 		p = p_;
 	}
 
+	/**
+	 * Gets the terrain of this cell
+	 * 
+	 * @return
+	 */
 	public int getTerrain() {
 		return terrain;
 	}
 
+	/**
+	 * Sets the terrain of this cell
+	 * 
+	 * @param terrain_
+	 */
 	public void setTerrain(int terrain_) {
 		terrain = terrain_;
 	}
 
+	/**
+	 * Returns the owner of this cell
+	 * 
+	 * @return
+	 */
 	public int getOwner() {
 		return whoOwns;
 	}
 
+	/**
+	 * Sets the owner for this cell
+	 * 
+	 * @param whoOwns_
+	 */
 	public void setOwner(int whoOwns_) {
 		whoOwns = whoOwns_;
 	}
 
+	/**
+	 * Returns True if this is an impassable cell
+	 * 
+	 * @return
+	 */
 	public boolean isImpassable() {
-		return getTerrain() == TERRAIN_MOUNTAIN
-				|| getTerrain() == TERRAIN_WATER;
+		return getTerrain() == Constants.TERRAIN_MOUNTAIN
+				|| getTerrain() == Constants.TERRAIN_WATER;
 	}
 
+	/**
+	 * Returns the co-ordinates for this cell
+	 * 
+	 * @return
+	 */
 	public Point getPoint() {
 		return p;
 	}
@@ -70,6 +118,78 @@ public class Cell {
 	@Override
 	public String toString() {
 		return "[ ( " + p.x + "," + p.y + ") , Terrain =" + terrain + " ]";
+	}
+
+	/**
+	 * Returns the cell to the north
+	 * 
+	 * @return
+	 */
+	public Cell getNorthCell() {
+		return null;
+	}
+
+	/**
+	 * Returns the cell to the east
+	 * 
+	 * @return
+	 */
+	public Cell getEastCell() {
+		return null;
+	}
+
+	/**
+	 * Returns the cell to the south
+	 * 
+	 * @return
+	 */
+	public Cell getSouthCell() {
+		return null;
+	}
+
+	/**
+	 * Returns the cell to the west
+	 * 
+	 * @return
+	 */
+	public Cell getWestCell() {
+		return null;
+	}
+
+	/**
+	 * Returns the cell to the northeast
+	 * 
+	 * @return
+	 */
+	public Cell getNorthEastCell() {
+		return null;
+	}
+
+	/**
+	 * Returns the cell to the northwest
+	 * 
+	 * @return
+	 */
+	public Cell getNorthWestCell() {
+		return null;
+	}
+
+	/**
+	 * Returns the cell to the southeast
+	 * 
+	 * @return
+	 */
+	public Cell getSouthEastCell() {
+		return null;
+	}
+
+	/**
+	 * Returns the cell to the southwest
+	 * 
+	 * @return
+	 */
+	public Cell getSouthWestCell() {
+		return null;
 	}
 
 }
--- trunk/ explorandum-g2 --username ltazmin/Explorandum/src/explorandum/f09/g2/Grid.java
+++ trunk/ explorandum-g2 --username ltazmin/Explorandum/src/explorandum/f09/g2/Grid.java
@@ -1,26 +1,53 @@
 package explorandum.f09.g2;
 
 import java.awt.Point;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 
-import explorandum.GameConstants;
-import explorandum.Move;
-
+/**
+ * Class for the Grid/Map
+ * @author sharadh
+ *
+ */
 public class Grid {
 
-	private static HashMap<Point, Cell> gridMap = new HashMap<Point, Cell>();
+	/**
+	 * Hashmap with key as Point and value as Cell
+	 */
+	private HashMap<Point, Cell> gridMap;
+	
+	/**
+	 * Private Constructor
+	 */
+	public Grid() {
+		gridMap = new HashMap<Point, Cell>();
+	}
 
-	public static Cell getCell(Point p) {
+
+	/**
+	 * Clears all present information
+	 */
+	public void clear(){
+		gridMap.clear();
+	}
+	/**
+	 * Returns the cell represented by co-ordiantes given by point P
+	 * @param p
+	 * @return
+	 */
+	public Cell getCell(Point p) {
 		return gridMap.get(p);
 	}
 
-	public static void putCell(Point p, Cell c) {
+	/**
+	 * Puts the cell c into the grid, indexed by point p
+	 * @param p
+	 * @param c
+	 */
+	public void putCell(Point p, Cell c) {
 		gridMap.put(p, c);
 	}
 
-	public static HashSet<Point> campSites = new HashSet<Point>();
+/*	public static HashSet<Point> campSites = new HashSet<Point>();
 
 	public static Point getClosestCampSite(Point p) {
 		return null;
@@ -29,8 +56,15 @@ public class Grid {
 	public static void putCell(Point p) {
 		campSites.add(p);
 	}
-
-	public static void updateVisitedCellInformation(Point p, int turn,
+*/
+	/**
+	 * Updates the information for a cell which has been visited by player
+	 * @param p
+	 * @param turn
+	 * @param isFootPrintPresent
+	 * @param isExplorerPresent
+	 */
+	public void updateVisitedCellInformation(Point p, int turn,
 			boolean isFootPrintPresent, boolean isExplorerPresent) {
 		Cell cell = getCell(p);
 		if (cell != null) {
@@ -40,189 +74,46 @@ public class Grid {
 			cell.setVisitationTurn(turn);
 
 			// Update Ownership
-			if (cell.getOwner() != Cell.OWNERSHIP_US) {
+			if (cell.getOwner() != Constants.OWNER_BY_US) {
 				if (isFootPrintPresent) {
-					cell.setOwner(Cell.OWNERSHIP_THEM);
+					cell.setOwner(Constants.OWNED_BY_THEM);
 				} else {
-					cell.setOwner(Cell.OWNERSHIP_US);
+					cell.setOwner(Constants.OWNER_BY_US);
 				}
 			}
 		} else {
 			cell = new Cell(p);
 			cell.setVisited(true);
 			cell.setVisitationTurn(turn);
-			cell.setOwner(Cell.OWNERSHIP_US);
+			cell.setOwner(Constants.OWNER_BY_US);
 			putCell(p, cell);
 		}
 	}
 
-	public static void updateSeenCellInformation(Point p, int terrain,
+	/**
+	 * Updates the information of a cell which has been seen by player
+	 * @param p
+	 * @param terrain
+	 * @param isExplorerSeen
+	 */
+	public void updateSeenCellInformation(Point p, int terrain,
 			boolean isExplorerSeen) {
 		Cell cell = getCell(p);
 		if (cell != null) {
 
 			if (!cell.isVisited() && isExplorerSeen) {
-				cell.setOwner(Cell.OWNERSHIP_THEM);
+				cell.setOwner(Constants.OWNED_BY_THEM);
 			}
 
 		} else {
 			cell = new Cell(p);
 			cell.setTerrain(terrain);
 			if (isExplorerSeen) {
-				cell.setOwner(Cell.OWNERSHIP_THEM);
+				cell.setOwner(Constants.OWNED_BY_THEM);
 			}
 			putCell(p, cell);
 		}
 	}
 
-	public static Move getMoveFrom(Point point_) {
 
-		ArrayList<Cell> unVisitedNeighbours = new ArrayList<Cell>();
-		ArrayList<Cell> visitedNeighbours = new ArrayList<Cell>();
-		int maxScore = Integer.MIN_VALUE;
-		int maxScoreIndex = 0;
-		int oldestVisitIndex = 0;
-		int oldestVisit = Integer.MAX_VALUE;
-		for (int i = 0; i < 8; i++) {
-			Cell c = getCell(new Point(point_.x + GameConstants._dx[i + 1],
-					point_.y + GameConstants._dy[i + 1]));
-			if (c != null) {
-				if (!c.isImpassable()) {
-					if (c.isVisited()) {
-						visitedNeighbours.add(c);
-						if (c.getVisitationTurn() < oldestVisit) {
-							oldestVisitIndex = visitedNeighbours.size() - 1;
-							oldestVisit = c.getVisitationTurn();
-
-						}
-					} else {
-						unVisitedNeighbours.add(c);
-						int cellScore = getCellScore(c);
-						if (cellScore > maxScore) {
-							maxScoreIndex = unVisitedNeighbours.size() - 1;
-							maxScore = cellScore;
-
-						}
-					}
-				}
-			}
-		}
-
-		System.err.println("MaxScore" + maxScore);
-		if (unVisitedNeighbours.size() != 0) {
-
-			Cell to = unVisitedNeighbours.get(maxScoreIndex);
-			System.out.println(to);
-			return determineMove(point_, to.getPoint());
-		} else /* if(visitedNeighbours.size() != 0) */{
-			Cell to = visitedNeighbours.get(oldestVisitIndex);
-			return determineMove(point_, to.getPoint());
-		}
-	}
-
-	private static Move determineMove(Point from_, Point to_) {
-		// int diffX = from_.x - to_.x;
-		// int diffY = from_.y - to_.y;
-
-		int diffX = to_.x - from_.x;
-		int diffY = to_.y - from_.y;
-
-		switch (diffY) {
-		case 1:
-			switch (diffX) {
-			case 1:
-				return new Move(GameConstants.SOUTHEAST);
-			case 0:
-				return new Move(GameConstants.SOUTH);
-			case -1:
-				return new Move(GameConstants.SOUTHWEST);
-
-			}
-		case 0:
-			switch (diffX) {
-			case 1:
-				return new Move(GameConstants.EAST);
-			case -1:
-				return new Move(GameConstants.WEST);
-
-			}
-		case -1:
-			switch (diffX) {
-			case 1:
-				return new Move(GameConstants.NORTHEAST);
-			case 0:
-				return new Move(GameConstants.NORTH);
-			case -1:
-				return new Move(GameConstants.NORTHWEST);
-
-			}
-		}
-		return null;
-	}
-
-	public static int getCellScore(Cell c_) {
-		int px = c_.getPoint().x;
-		int py = c_.getPoint().y;
-		int score = 0;
-		System.out.println(c_);
-
-		// Edge Neighbours
-		for (int i = 0; i < Constants.edgeNeighborOffsets.length; i++) {
-			Cell c = getCell(new Point(
-					px + Constants.edgeNeighborOffsets[i][0], py
-							+ Constants.edgeNeighborOffsets[i][1]));
-			System.out.println("I : " + i + " " + c);
-			if (c != null) {
-				switch (c.getTerrain()) {
-				case Cell.TERRAIN_LAND:
-					score += 1;
-					break;
-
-				case Cell.TERRAIN_WATER:
-					score += 3;
-					break;
-
-				case Cell.TERRAIN_MOUNTAIN:
-					score += 2;
-					break;
-				}
-				score += 1;
-
-				if (c.isVisited()) {
-					score -= 2;
-				}
-			}
-
-		}
-
-		// Edge Neighbours
-		for (int i = 0; i < Constants.vertexNeighborOffsets.length; i++) {
-			Cell c = getCell(new Point(px
-					+ Constants.vertexNeighborOffsets[i][0], py
-					+ Constants.vertexNeighborOffsets[i][1]));
-			System.out.println("I : " + i + " " + c);
-			if (c != null) {
-				switch (c.getTerrain()) {
-				case Cell.TERRAIN_LAND:
-					score += 1;
-					break;
-
-				case Cell.TERRAIN_WATER:
-					score += 3;
-					break;
-
-				case Cell.TERRAIN_MOUNTAIN:
-					score += 2;
-					break;
-				}
-				if (c.isVisited()) {
-					score -= 1;
-
-				}
-			}
-
-		}
-		System.out.println("Score " + score);
-		return score;
-	}
 }
