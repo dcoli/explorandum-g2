76cee8fd2dfb62aedfcc9c24d4ec74ec4a263510
sharadh <sharadh@e1d36b18-a32f-11de-bf1c-ef771b8a44ac>
2009-09-21 16:10:02 +0000

    Updated

---  explorandum-g2 --username ltazmin/Explorandum/src/explorandum/f09/g2/Helper.java
+++  explorandum-g2 --username ltazmin/Explorandum/src/explorandum/f09/g2/Helper.java
@@ -285,18 +285,70 @@ public class Helper {
 			}
 		}
 
-		System.err.println("MaxScore" + maxScore);
+		//System.out.println("MaxScore" + maxScore);
 		
 		if (unVisitedNeighbours.size() != 0) {
 
 			Cell to = unVisitedNeighbours.get(maxScoreIndex);
-			System.out.println(to);
+			//System.out.println(to);
 			return Helper.getMove(point_, to.getPoint());
 		} else /* if(visitedNeighbours.size() != 0) */{
 			
 			//return Helper.getTargetedMoveFrom(point_, grid_);
 			Cell to = visitedNeighbours.get(oldestVisitIndex);
 			return Helper.getMove(point_, to.getPoint());
+		}
+	}
+
+	public static Move getNextMoveTowardsTarget(Point currentLocation_,
+			Grid grid_, TargetInfo target_) {
+
+		int dx = target_.getTarget().x - currentLocation_.x;
+		int dy = target_.getTarget().y - currentLocation_.y;
+
+		int[][] possOffsets = new int[3][2];
+		if (dx == 0) {
+			possOffsets[0] = new int[] { 0, (int) Math.signum(dy) * 1 };
+			possOffsets[1] = new int[] { 1, (int) Math.signum(dy) * 1 };
+			possOffsets[2] = new int[] { -1, (int) Math.signum(dy) * 1 };
+		} else if (dy == 0) {
+			possOffsets[0] = new int[] { (int) Math.signum(dx) * 1, 0 };
+			possOffsets[1] = new int[] { (int) Math.signum(dx) * 1, 1 };
+			possOffsets[2] = new int[] { (int) Math.signum(dx) * 1, -1 };
+		} else {
+			possOffsets[0] = new int[] { (int) Math.signum(dy) * 1,
+					(int) Math.signum(dy) * 1 };
+			possOffsets[1] = new int[] { 0, (int) Math.signum(dy) * 1 };
+			possOffsets[2] = new int[] { (int) Math.signum(dx) * 1, 0 };
+
+		}
+
+		ArrayList<Cell> possMoves = new ArrayList<Cell>();
+		int maxScore = Integer.MIN_VALUE;
+		int maxScoreIndex = 0;
+		for (int i = 0; i < possOffsets.length; i++) {
+			Cell c = grid_.getCell(currentLocation_, possOffsets[i][0],
+					possOffsets[i][1]);
+			//System.out.println(c.getPoint() + " " + c.getScore());
+			if (!c.isImpassable()) {
+				possMoves.add(c);
+				//int cellScore = c.getScore() - c.getLastTurnVisited() + ;
+				int cellScore = c.getScore() - c.getDistanceFrom(target_.getTarget());
+				if (cellScore > maxScore) {
+					maxScoreIndex = possMoves.size() - 1;
+					maxScore = cellScore;
+				}
+			}
+		}
+
+		if (possMoves.size() != 0) {
+
+			Cell to = possMoves.get(maxScoreIndex);
+			//System.out.println(to);
+			return Helper.getMove(currentLocation_, to.getPoint());
+		} else {
+			// TODO Auto-generated method stub
+			return null;
 		}
 	}
 
